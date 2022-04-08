658a7571e33caa5f0a678cb80fceb4dc16597a51
perrotth1 <perrotth1@hawkmail.newpaltz.edu>
2022-04-08 12:25:46 -0400

    added test for add+listDVDs

--- DVDLibrary/src/test/java/com/hjp/dvdlibrary/dao/DVDLibraryDaoFileImplTest.java
+++ DVDLibrary/src/test/java/com/hjp/dvdlibrary/dao/DVDLibraryDaoFileImplTest.java
@@ -9,7 +9,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import com.hjp.dvdlibrary.dao.*;
 import com.hjp.dvdlibrary.dto.*;
 import java.io.FileWriter;
-
+import java.util.List;
 /**
  *
  * @author Henry
@@ -19,7 +19,8 @@ public class DVDLibraryDaoFileImplTest {
     /**
      *  TEST PLAN
      * 
-     *  testAddGetDVD(DVD dvd) - Add a DVD, get it and prove it was added by checking the data is equal
+     *  testAddGetDVD() - Add a DVD, get it and prove it was added by checking the data is equal
+     * testAddGetAllDVD()
      */
     
     private DVDLibraryDao testDao;
@@ -58,6 +59,26 @@ public class DVDLibraryDaoFileImplTest {
         
         //ASSERT
         assertEquals(newDVD, retrievedDVD);
+    }
+    
+    @Test
+    public void testAddGetAllDVD() throws Exception{
+        //ARRANGE
+        DVD dvd1 = new DVD("title1","release","rating","director","studio","note");
+        DVD dvd2 = new DVD("title2","release2","rating2", "director2","studio2","note2");
+    
+        testDao.addDVD(dvd1);
+        testDao.addDVD(dvd2);
+        
+        //ACT
+        List<DVD> retrievedList = testDao.listDVD();
+        
+        //ASSERT
+        assertNotNull( retrievedList, "List should not be null");
+        assertEquals( 2, retrievedList.size(), "List should be size 2 ");
+        
+        assertTrue( retrievedList.contains(dvd1) , "List should contain title1");
+        assertTrue( retrievedList.contains(dvd2) , "List should contain title2" );
     }
     
 }
