f7520c9278a4ff07f0280d610dcaf87bb0c3aa47
perrotth1 <perrotth1@hawkmail.newpaltz.edu>
2022-04-11 12:27:07 -0400

    created

---
+++ DVDLibrary/src/main/java/com/hjp/dvdlibrary/dao/DVDLibraryDaoAgg.java
@@ -0,0 +1,20 @@
+package com.hjp.dvdlibrary.dao;
+
+/**
+ *
+ * @author Henry
+ */
+public interface DVDLibraryDaoAgg extends DVDLibraryDao {
+    /**
+     * 
+    Find all movies released in the last N years
+    Find all the movies with a given MPAA rating
+    Find all the movies by a given director
+    When searching by director, the movies should be sorted into separate data structures by MPAA rating.
+    Find all the movies released by a particular studio
+    Find the average age of the movies in the collection
+    Find the newest movie in your collection
+    Find the oldest movie in your collection
+    Find the average number of notes associated with movies in your collection
+     */
+}
