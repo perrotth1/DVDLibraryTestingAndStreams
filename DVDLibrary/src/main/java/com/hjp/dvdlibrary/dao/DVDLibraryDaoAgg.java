c1bf6a904dd17def69db8897896cfbd0e3bd84e7
magpantaynic <20709370+magpantaynic@users.noreply.github.com>
2022-04-11 13:50:13 -0400

    merge

--- DVDLibrary/src/main/java/com/hjp/dvdlibrary/dao/DVDLibraryDaoAgg.java
+++ DVDLibrary/src/main/java/com/hjp/dvdlibrary/dao/DVDLibraryDaoAgg.java
@@ -1,27 +1,25 @@
 package com.hjp.dvdlibrary.dao;
 
-import com.hjp.dvdlibrary.dto.DVD;
-import java.util.List;
-
 /**
  *
  * @author Henry
  */
 public interface DVDLibraryDaoAgg extends DVDLibraryDao {
     /**
-     *
+     * 
+    Henry:
     Find all movies released in the last N years
     Find all the movies with a given MPAA rating
     Find all the movies by a given director
+
+    Jeonghoon:
     When searching by director, the movies should be sorted into separate data structures by MPAA rating.
     Find all the movies released by a particular studio
     Find the average age of the movies in the collection
+
+    Nicole:
     Find the newest movie in your collection
     Find the oldest movie in your collection
     Find the average number of notes associated with movies in your collection
      */
-
-    public List<DVD> searchDVDByStudio(String _studio) throws DVDLibraryDaoException;
-
-    public double getAverageAgeOfMovies();
 }
--- DVDLibrary/src/test/java/com/hjp/dvdlibrary/dao/DVDLibraryDaoFileImplTest.java
+++ DVDLibrary/src/test/java/com/hjp/dvdlibrary/dao/DVDLibraryDaoFileImplTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
-import com.hjp.dvdlibrary.dao.*;
+//import com.hjp.dvdlibrary.dao.*;
 import com.hjp.dvdlibrary.dto.*;
 import java.io.FileWriter;
 import java.util.List;
